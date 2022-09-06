61a64b39ff40200fe189960ee624f6536f4037fc
204G1A0502 <94514001+204G1A0502@users.noreply.github.com>
2022-09-06 12:11:34 +0530

    Create FCFS.c
    D Abhiram Created FCFS.c

---
+++ exp1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: D.Abhiram
+   Roll No: 204G1A0502
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 6-08-2022
+   Date of Execution: 6-08-2022
+*/
